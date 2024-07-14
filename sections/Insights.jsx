833d33d7f419966f0f5008bea08a1c69bf3e6a9b
Albérico Eduardo <albericoeduardo202@gmail.com>
2024-07-14 11:27:22 -0300

    build:  ATT Dependencies & ESLint

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,27 +1,26 @@
 'use client';
 
-
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { TypingText, TitleText, InsightCard } from '../components';
-import {staggerContainer} from '../utils/motion';
-import {insights} from '../constants';
+import { staggerContainer } from '../utils/motion';
+import { insights } from '../constants';
 import styles from '../styles';
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
       variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
-      viewport={{once: false, amount: 0.25}}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title='Insight' textStyles='text-center'/>
-      <TitleText title='Insight about metaverse' textStyles='text-center'/>
-      <div className='mt-[50px] flex flex-col gap-[30px]'>
+      <TypingText title="Insight" textStyles="text-center" />
+      <TitleText title="Insight about metaverse" textStyles="text-center" />
+      <div className="mt-[50px] flex flex-col gap-[30px]">
         {insights.map((insight, index) => (
-          <InsightCard 
+          <InsightCard
             key={`insight-${index}`}
             {...insight}
             index={index + 1}
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,30 +1,30 @@
 'use client';
 
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { TypingText, TitleText, NewFeatures } from '../components';
-import {staggerContainer, fadeIn, planetVariants} from '../utils/motion';
-import {newFeatures} from '../constants';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { newFeatures } from '../constants';
 import styles from '../styles';
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
       variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
-      viewport={{once: false, amount: 0.25}}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
-        className='flex-[0.75] flex justify-center flex-col'
+        className="flex-[0.75] flex justify-center flex-col"
       >
-        <TypingText title="| What's New?"/>
+        <TypingText title="| What's New?" />
         <TitleText title={<>What's new about Metaversus?</>} />
-        <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
-          {newFeatures.map((feature, index) => (
-            <NewFeatures 
+        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+          {newFeatures.map((feature) => (
+            <NewFeatures
               key={feature.title}
               {...feature}
             />
@@ -35,10 +35,10 @@ const WhatsNew = () => (
         variants={planetVariants('right')}
         className={`flex-1 ${styles.flexCenter}`}
       >
-        <img 
-          src="/whats-new.png" 
+        <img
+          src="/whats-new.png"
           alt="whats-new"
-          className='w-[90%] h-[90%] object-contain' 
+          className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
     </motion.div>
